02e8a88e6bc49e619cf6cd29ceef24954510f409
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-02 18:52:18 +0200

    register new generic models

--- homepage/portfolio/admin.py
+++ homepage/portfolio/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import Project, Collection, Photo, Message, Contact
+from .models import Project, Collection, Photo, Message, Contact, BaseEntry, Category
+
 # Register your models here.
+
+
+
 
 admin.site.register(Project)
 admin.site.register(Collection)
 admin.site.register(Photo)
 admin.site.register(Message)
 admin.site.register(Contact)
+admin.site.register(BaseEntry)
+admin.site.register(Category)
