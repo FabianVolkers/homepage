7f27a13c174c65a70485300cf7500fc7fac6e6c9
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-03 18:22:45 +0200

    update admin to reflect model changes

--- homepage/portfolio/admin.py
+++ homepage/portfolio/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 
-from .models import Project, Collection, Photo, Message, Contact, BaseEntry, Category
+from .models import Message, Contact, BaseEntry, Section, SectionType, NavLink, Setting, FooterLink, Icon
 
 # Register your models here.
 
 
 
 
-admin.site.register(Project)
+""" admin.site.register(Project)
 admin.site.register(Collection)
-admin.site.register(Photo)
+admin.site.register(Photo) """
 admin.site.register(Message)
 admin.site.register(Contact)
 admin.site.register(BaseEntry)
-admin.site.register(Category)
+admin.site.register(Section)
+admin.site.register(SectionType)
+admin.site.register(NavLink)
+admin.site.register(Setting)
+admin.site.register(FooterLink)
+admin.site.register(Icon)
