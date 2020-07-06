3af088afd09419fc2139a9083786f9ac161dac96
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-07 01:45:35 +0200

    add admin models and views to manage translations, including db cleanup

--- homepage/portfolio/admin.py
+++ homepage/portfolio/admin.py
@@ -1,24 +1,108 @@
 from django.contrib import admin
+from django.conf import settings
 
-from .models import Message, Contact, BaseEntry, Section, SectionType, NavLink, Setting, FooterLink, Icon, Page, TranslationsGroup, SectionCommon
+from .models import *
+# Message, Contact, CollectionItem, Section, SectionType, Setting, Icon, Page, SectionCommon, NavLink, FooterLink, CollectionItem, LinkEdit # TranslationsGroup
 
 # Register your models here.
 
 
+""" 
+TODO: 
+- TranslationGroups Admin view with children
 
+"""
+
+class SlugAdmin(admin.ModelAdmin):
+    prepopulated_fiels = {
+        "slug": ("name",)
+        }
+
+class NavLinkInline(admin.TabularInline):
+    model = NavLink
+    readonly_fields = ('url',)
+
+class FooterLinkInline(admin.TabularInline):
+    model = FooterLink
+    readonly_fields = ('url',)
+
+class LinkAdmin(admin.ModelAdmin):
+    inlines = [
+        NavLinkInline,
+        FooterLinkInline
+    ]
+class TranslationInline(admin.StackedInline):
+    max_num = len(settings.LANGUAGES)
+    
+    
+    """ def get_prepopulated_fields(self, request, obj=None):
+        translated = self.model.objects.filter(translations_group__id=obj.id)
+        langs = settings.LANGUAGES
+        for lang in translated:
+            idx = [y[0] for y in langs].index(lang.lang)
+            del langs[idx]
+        
+        return {"lang": next(iter(langs))} """
+    
+class SectionCommonInline(admin.TabularInline):
+    model = SectionCommon 
+class SectionInline(TranslationInline):
+    model = Section
+
+class SectionCommonAdmin(SlugAdmin):
+    inlines = [
+        SectionInline
+    ]
+
+class CollectionItemAdmin(admin.ModelAdmin):
+    model = CollectionItem
+    list_display = ('name', 'lang')
+
+class CollectionItemInline(TranslationInline):
+    model = CollectionItem
+
+
+class CollectionItemCommonAdmin(SlugAdmin):
+
+    inlines = [
+        CollectionItemInline
+    ]
+
+    
+
+class TranslationAdmin(admin.ModelAdmin):
+
+    inlines = [
+        SectionInline,
+        CollectionItemInline
+    ]
+    def get_formsets_with_inlines(self, request, obj=None):
+        for inline in self.get_inline_instances(request, obj):
+            if obj != None and inline.model.objects.filter(translations_group__id=obj.id, translations_group__isnull=False).count() > 0:
+                yield inline.get_formset(request, obj), inline
+            elif obj == None:
+                yield inline.get_formset(request, obj), inline
+
+class PageAdmin(SlugAdmin):
+    readonly_fields = ('view_name',)
+    fields = ('name', 'template_name', 'slug', 'page_type', 'overwrite_view_name', 'view_name')
+    
 
 """ admin.site.register(Project)
 admin.site.register(Collection)
 admin.site.register(Photo) """
 admin.site.register(Message)
 admin.site.register(Contact)
-admin.site.register(BaseEntry)
-admin.site.register(Section)
+admin.site.register(CollectionItem, CollectionItemAdmin)
+admin.site.register(Section, SlugAdmin)
 admin.site.register(SectionType)
 admin.site.register(NavLink)
 admin.site.register(Setting)
 admin.site.register(FooterLink)
+admin.site.register(LinkEdit, LinkAdmin)
 admin.site.register(Icon)
-admin.site.register(Page)
-admin.site.register(TranslationsGroup)
-admin.site.register(SectionCommon)
+admin.site.register(PageType)
+admin.site.register(Page, PageAdmin)
+#admin.site.register(TranslationsGroup, TranslationAdmin)
+admin.site.register(SectionCommon, SectionCommonAdmin)
+admin.site.register(CollectionItemCommon, CollectionItemCommonAdmin)
--- homepage/portfolio/models.py
+++ homepage/portfolio/models.py
@@ -5,6 +5,15 @@ from django.db import models
 from django.core.mail import send_mail, EmailMessage, BadHeaderError, EmailMultiAlternatives
 from django.conf import settings
 from django.template.loader import get_template
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
+
+""" 
+TODO: 
+- Link abstract base class
+- AbstractTranslatable BaseEntry with commonEntry and TranslationGroup
+
+"""
 
 views = [
     ('portfolio:project-detail', 'Project Detail View'),
@@ -12,8 +21,6 @@ views = [
     ('portfolio:photo-detail', 'Photo View'),
     ('portfolio:entry-detail', 'Base View')
 ]
-
-
 
 page_positions = []
 for h in range(settings.MAX_PAGES):
@@ -23,13 +30,19 @@ section_postitions = []
 for i in range(settings.MAX_SECTIONS):
     section_postitions.append((i, i))
 
-navlink_positions = []
+link_positions = []
 for j in range(settings.MAX_NAVLINKS):
-    navlink_positions.append((j, j))
-
-footerlink_positions = []
+    link_positions.append((f"navbar-{j}", f"navbar {j}"))
 for k in range(settings.MAX_FOOTERLINKS):
-    footerlink_positions.append((k, k))
+    link_positions.append((f"footer-{k}", f"footer {k}"))
+
+sociallink_positions = []
+#for l in range()
+link_types = [
+    ('page', 'Page'),
+    ('section', 'Section'),
+    ('external', 'External')
+]
 # Create your models here.
 class Setting(models.Model):
     key = models.CharField(max_length=256)
@@ -44,38 +57,62 @@ class Icon(models.Model):
     def __str__(self):
         return self.name
 
-class Link(models.Model):
-    name = models.CharField(max_length=12)
-    url = models.CharField(max_length=256)
-    view = models.CharField(max_length=64, default='portfolio:project-detail', choices=views)
-    def __str__(self):
-        return self.url
+class AbstractTranslatable(models.Model):
 
-class FooterLink(Link):
-    position = models.IntegerField(null=True, choices=footerlink_positions, unique=True)
-    icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
-
-class NavLink(Link):
-    #link_positions = range(1, len(NavLink.objects.all()))
-    position = models.IntegerField(null=True, choices=navlink_positions, unique=True)
-
-class TranslationsGroup(models.Model):
-     name = models.CharField(max_length=24)
-
-     def __str__(self):
-         return self.name
-
-class Translatable(models.Model):
-    translations_group = models.ForeignKey(TranslationsGroup, on_delete=models.SET_NULL, null=True)
     lang = models.CharField(max_length=5, choices=settings.LANGUAGES, default=settings.LANGUAGE_CODE)
     class Meta:                                                             
         abstract = True
+        unique_together = ['common', 'lang']
     
+    def __str__(self):
+        return self.lang
+
+class PageType(models.Model):
+    name = models.CharField(max_length=24)
+
+    def __str__(self):
+        return self.name
+
+class PageCommon(models.Model):
+    template_name = models.CharField(max_length=64)
+    page_type = models.ForeignKey(PageType, on_delete=models.SET_NULL, null=True, blank=True)
+    view_name = models.CharField(max_length=64, editable=False)
+    overwrite_view_name = models.CharField(max_length=64, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('Page')
+        verbose_name_plural = _('Pages')
+
+    def __str__(self):
+        return self.name
 class Page(models.Model):
     name = models.CharField(max_length=24)
     template_name = models.CharField(max_length=64)
-    nav_bar_link = models.BooleanField(default=True)
-    nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, null=True)
+    slug = models.SlugField(null=True, blank=True)
+    page_type = models.ForeignKey(PageType, on_delete=models.SET_NULL, null=True, blank=True)
+    view_name = models.CharField(max_length=64, editable=False)
+    overwrite_view_name = models.CharField(max_length=64, null=True, blank=True)
+    #nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, default=1, null=True)
+
+    class Meta:
+        verbose_name = _('Single Page')
+        verbose_name_plural = _('Single Pages')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.name != 'Home':
+            self.slug = slugify(self.name)
+        else:
+            self.slug = ""
+
+        if self.overwrite_view_name != None:
+            self.view_name = self.overwrite_view_name
+        else:
+            self.view_name = f"portfolio:{self.slug}-{self.page_type.name}"
+        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+ 
 
 class SectionType(models.Model):
     name = models.CharField(max_length=64)
@@ -90,18 +127,25 @@ class SectionCommon(models.Model):
     friendly_name = models.CharField(max_length=64)
     section_type = models.ForeignKey(SectionType, on_delete=models.SET_NULL, null=True)
     position = models.IntegerField(null=True, choices=section_postitions, unique=True)
-    nav_bar_link = models.BooleanField(default=True)
-    nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, null=True)
+    page = models.ForeignKey(Page, on_delete=models.SET_NULL, null=True, blank=True, related_name='sections')
+    #nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, default=1, null=True)
+
+    class Meta:
+        verbose_name = _('Page Section')
+        verbose_name_plural = _('Page Sections')
 
     def __str__(self):
         return self.friendly_name
-class Section(Translatable):
+class Section(AbstractTranslatable):
     #section_positions = range(1, len(Section.objects.all()))
     name = models.CharField(max_length=64)
-    slug = models.SlugField(editable=False)
+    slug = models.SlugField()
     description = models.TextField(null=True, blank=True)
     common = models.ForeignKey(SectionCommon, related_name='sections', on_delete=models.CASCADE, null=True)
     
+    class Meta:
+        verbose_name = _('Section Translation')
+        verbose_name_plural = _('Sections Translations')
 
     def __str__(self):
         return self.name
@@ -112,32 +156,103 @@ class Section(Translatable):
         if self.position == None:
             self.position = self.section_type.default_position """
 
-        self.slug = generate_slug(self.name)
-        if self.common.nav_bar_link:
+        self.slug = slugify(self.name)
+        """ if self.common.nav_bar_link:
             try:
                 NavLink.objects.get(name=self.slug)
             except models.ObjectDoesNotExist:
                 position = len(NavLink.objects.all())
                 navlink = NavLink(name=self.slug, url=f'/#{self.slug}', position=position)
                 navlink.save()
-                self.common.nav_link = navlink
-        super().save()
+                self.common.nav_link = navlink """
+        return super().save()
+
+
+class AbstractLink(models.Model):
+    name = models.CharField(max_length=12)
+    url = models.CharField(max_length=256, editable=False, blank=True, default='/')
+    view = models.CharField(max_length=64, default='portfolio:project-detail', choices=views)
+    position = models.CharField(max_length=8, null=True, choices=link_positions, unique=True)
+    link_for = models.CharField(max_length=24, choices=link_types, null=True)
+    section = models.ForeignKey(Section, null=True, blank=True, on_delete=models.SET_NULL)
+    page = models.ForeignKey(Page, null=True, blank=True, on_delete=models.SET_NULL)
+    external = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.url
+
+    def save(self):
+        if self.link_for == 'section':
+            self.url = f"{self.section.page.slug}/#{self.section.slug}"
+        elif self.link_for == 'page':
+            self.url = f"/{self.page.slug}"
+        elif self.link_for == 'external':
+            self.url = self.external
+        return super.save()
+
+    class Meta:
+        abstract = True
+        #verbose_name = 'ModelName'
+        #verbose_name_plural = 'ModelNames'
+
+class LinkEdit(models.Model):
+
+    class Meta:
+        verbose_name = _('Page Link')
+        verbose_name_plural = _('Page Links')
+
+class SocialLink(AbstractLink):
+    icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
+    link_edit = models.ForeignKey(LinkEdit, on_delete=models.SET_NULL, null=True, related_name='social_links')
+
+    class Meta:
+        verbose_name = _('Social Link')
+        verbose_name_plural = _('Social Links')
+
+class FooterLink(AbstractLink):
+    icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
+    link_edit = models.ForeignKey(LinkEdit, on_delete=models.SET_NULL, null=True, related_name='footer_links')
+
+    class Meta:
+        verbose_name = _('Footer Link')
+        verbose_name_plural = _('Footer Links')
+
+class NavLink(AbstractLink):
+    link_edit = models.ForeignKey(LinkEdit, on_delete=models.SET_NULL, null=True, related_name='navbar_links')
     
-class BaseEntry(Translatable):
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, editable=False)
-    description = models.TextField()
+    class Meta:
+        verbose_name = _('Navigation Link')
+        verbose_name_plural = _('Navigation Links')
+
+
+class CollectionItemCommon(models.Model):
+    friendly_name = models.CharField(max_length=64)
     image = models.ImageField(upload_to='images')
     created = models.DateTimeField()
     spotlight = models.BooleanField()
     parent_section = models.ForeignKey(Section, on_delete=models.SET_NULL, null=True)
     detail_view = models.CharField(max_length=64, default='portfolio.views.BaseEntryView', choices=views)
 
+    class Meta:
+        verbose_name = _('Collection Item')
+        verbose_name_plural = _('Collection Items')
+
     def __str__(self):
-        return self.name
+        return self.friendly_name
+
+class CollectionItem(AbstractTranslatable):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, editable=False)
+    description = models.TextField()
+    common = models.ForeignKey(CollectionItemCommon, related_name='collection_items', on_delete=models.CASCADE, null=True)
+    image_alttext = models.TextField(default="")
+    class Meta:
+        verbose_name = _('Item Translation')
+        verbose_name_plural = _('Item Translations')
+
 
     def save(self):
-        self.slug = generate_slug(self.name)
+        self.slug = slugify(self.name)
         super().save()
 """ 
 class Project(models.Model):
@@ -152,7 +267,7 @@ class Project(models.Model):
         return self.name
 
     def save(self):
-        self.slug = generate_slug(self.name)
+        self.slug = slugify(self.name)
         super().save()
 
 class Collection(models.Model):
@@ -166,7 +281,7 @@ class Collection(models.Model):
         return self.name
 
     def save(self):
-        self.slug = generate_slug(self.name)
+        self.slug = slugify(self.name)
         super().save()
 
 class Photo(models.Model):
@@ -180,7 +295,7 @@ class Photo(models.Model):
         return self.name
 
     def save(self):
-        self.slug = generate_slug(self.name)
+        self.slug = slugify(self.name)
         super().save()
  """
 
@@ -242,11 +357,3 @@ class Message(models.Model):
         # TODO: Check if email was sent successfi;;y
         self.sent = True
 
-
-def generate_slug(name):
-    REPLACE_CHARS = [' ', ',', '.', "'", '"', ':', ';', '/', '|', '\\']
-
-    for char in REPLACE_CHARS:
-        name = name.replace(char, "-")
-
-    return name.lower()
