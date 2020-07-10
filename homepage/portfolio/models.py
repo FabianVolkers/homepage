31e9e98fd8d77e9b77abcd93ab79e0cd90aac879
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-10 21:48:55 +0200

    finalise internationalisation and adjust for view changes

--- homepage/portfolio/models.py
+++ homepage/portfolio/models.py
@@ -16,10 +16,9 @@ TODO:
 """
 
 views = [
-    ('portfolio:project-detail', 'Project Detail View'),
-    ('portfolio:project-list', 'Project List View'),
-    ('portfolio:photo-detail', 'Photo View'),
-    ('portfolio:entry-detail', 'Base View')
+    ('portfolio:detail', 'Detail View'),
+    ('portfolio:collection', 'Collection View'),
+    ('portfolio:page', 'Page View'),
 ]
 
 page_positions = []
@@ -37,18 +36,22 @@ for k in range(settings.MAX_FOOTERLINKS):
     link_positions.append((f"footer-{k}", f"footer {k}"))
 
 sociallink_positions = []
-#for l in range()
+# for l in range()
 link_types = [
     ('page', 'Page'),
     ('section', 'Section'),
     ('external', 'External')
 ]
 # Create your models here.
+
+
 class Setting(models.Model):
     key = models.CharField(max_length=256)
     value = models.CharField(max_length=1024)
+
     def __str__(self):
         return f'{self.key}={self.value}'
+
 
 class Icon(models.Model):
     name = models.CharField(max_length=64)
@@ -57,41 +60,60 @@ class Icon(models.Model):
     def __str__(self):
         return self.name
 
+
 class AbstractTranslatable(models.Model):
 
-    lang = models.CharField(max_length=5, choices=settings.LANGUAGES, default=settings.LANGUAGE_CODE)
-    class Meta:                                                             
+    lang = models.CharField(
+        max_length=5, choices=settings.LANGUAGES, default=settings.LANGUAGE_CODE)
+
+    class Meta:
         abstract = True
-        unique_together = ['common', 'lang']
-    
+        unique_together = ['lang', 'slug']
+
     def __str__(self):
         return self.lang
 
+
 class PageType(models.Model):
     name = models.CharField(max_length=24)
+    template_name = models.CharField(max_length=64)
+    view_name = models.CharField(max_length=64, editable=False)
+    overwrite_view_name = models.CharField(
+        max_length=64, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+
+        if self.overwrite_view_name != None:
+            self.view_name = self.overwrite_view_name
+        else:
+            self.view_name = f"portfolio:{self.name}"
+
+        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+
+
 class PageCommon(models.Model):
+    friendly_name = models.CharField(max_length=64)
     template_name = models.CharField(max_length=64)
-    page_type = models.ForeignKey(PageType, on_delete=models.SET_NULL, null=True, blank=True)
-    view_name = models.CharField(max_length=64, editable=False)
-    overwrite_view_name = models.CharField(max_length=64, null=True, blank=True)
+    page_type = models.ForeignKey(
+        PageType, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = _('Page')
         verbose_name_plural = _('Pages')
 
     def __str__(self):
-        return self.name
-class Page(models.Model):
+        return self.friendly_name
+
+
+class Page(AbstractTranslatable):
     name = models.CharField(max_length=24)
-    template_name = models.CharField(max_length=64)
+
     slug = models.SlugField(null=True, blank=True)
-    page_type = models.ForeignKey(PageType, on_delete=models.SET_NULL, null=True, blank=True)
-    view_name = models.CharField(max_length=64, editable=False)
-    overwrite_view_name = models.CharField(max_length=64, null=True, blank=True)
+    common = models.ForeignKey(PageCommon, on_delete=models.CASCADE, null=True)
+
     #nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, default=1, null=True)
 
     class Meta:
@@ -102,47 +124,50 @@ class Page(models.Model):
         return self.name
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        if self.name != 'Home':
-            self.slug = slugify(self.name)
-        else:
-            self.slug = ""
+        super().save(force_insert, force_update, using, update_fields)
+        self.slug = slugify(self.name)
 
-        if self.overwrite_view_name != None:
-            self.view_name = self.overwrite_view_name
-        else:
-            self.view_name = f"portfolio:{self.slug}-{self.page_type.name}"
-        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
- 
 
 class SectionType(models.Model):
     name = models.CharField(max_length=64)
     template_name = models.CharField(max_length=64)
     default_position = models.IntegerField()
-    
 
     def __str__(self):
         return self.name
 
+
 class SectionCommon(models.Model):
     friendly_name = models.CharField(max_length=64)
-    section_type = models.ForeignKey(SectionType, on_delete=models.SET_NULL, null=True)
-    position = models.IntegerField(null=True, choices=section_postitions, unique=True)
-    page = models.ForeignKey(Page, on_delete=models.SET_NULL, null=True, blank=True, related_name='sections')
+    section_type = models.ForeignKey(
+        SectionType, on_delete=models.SET_NULL, null=True)
+    position = models.IntegerField(null=True, choices=section_postitions)
+    page = models.ForeignKey(
+        PageCommon,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='sections'
+    )
     #nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, default=1, null=True)
 
     class Meta:
         verbose_name = _('Page Section')
         verbose_name_plural = _('Page Sections')
+        unique_together = ['page', 'position']
 
     def __str__(self):
         return self.friendly_name
+
+
 class Section(AbstractTranslatable):
     #section_positions = range(1, len(Section.objects.all()))
     name = models.CharField(max_length=64)
     slug = models.SlugField()
+    detail_slug = models.SlugField()
     description = models.TextField(null=True, blank=True)
-    common = models.ForeignKey(SectionCommon, related_name='sections', on_delete=models.CASCADE, null=True)
-    
+    common = models.ForeignKey(
+        SectionCommon, related_name='sections', on_delete=models.CASCADE, null=True)
+
     class Meta:
         verbose_name = _('Section Translation')
         verbose_name_plural = _('Sections Translations')
@@ -170,30 +195,42 @@ class Section(AbstractTranslatable):
 
 class AbstractLink(models.Model):
     name = models.CharField(max_length=12)
-    url = models.CharField(max_length=256, editable=False, blank=True, default='/')
-    view = models.CharField(max_length=64, default='portfolio:project-detail', choices=views)
-    position = models.CharField(max_length=8, null=True, choices=link_positions, unique=True)
+    url = models.CharField(max_length=256, editable=False,
+                           blank=True, default='/')
+    view = models.CharField(
+        max_length=64, default='portfolio:detail', choices=views)
+    position = models.CharField(
+        max_length=8, null=True, choices=link_positions, unique=True)
     link_for = models.CharField(max_length=24, choices=link_types, null=True)
-    section = models.ForeignKey(Section, null=True, blank=True, on_delete=models.SET_NULL)
-    page = models.ForeignKey(Page, null=True, blank=True, on_delete=models.SET_NULL)
+    section = models.ForeignKey(
+        Section, null=True, blank=True, on_delete=models.SET_NULL)
+    page = models.ForeignKey(PageCommon, null=True,
+                             blank=True, on_delete=models.SET_NULL)
     external = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.url
 
-    def save(self):
-        if self.link_for == 'section':
-            self.url = f"{self.section.page.slug}/#{self.section.slug}"
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        """ if self.link_for == 'section':
+            self.url = f"{self.page.slug}/#{self.section.slug}"
         elif self.link_for == 'page':
-            self.url = f"/{self.page.slug}"
+            self.url = f"/{self.page.objects.select_related('common').get()slug}"
         elif self.link_for == 'external':
-            self.url = self.external
-        return super.save()
+            self.url = self.external """
+
+        return super().save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields
+        )
 
     class Meta:
         abstract = True
         #verbose_name = 'ModelName'
         #verbose_name_plural = 'ModelNames'
+
 
 class LinkEdit(models.Model):
 
@@ -201,25 +238,31 @@ class LinkEdit(models.Model):
         verbose_name = _('Page Link')
         verbose_name_plural = _('Page Links')
 
+
 class SocialLink(AbstractLink):
     icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
-    link_edit = models.ForeignKey(LinkEdit, on_delete=models.SET_NULL, null=True, related_name='social_links')
+    link_edit = models.ForeignKey(
+        LinkEdit, on_delete=models.SET_NULL, null=True, related_name='social_links')
 
     class Meta:
         verbose_name = _('Social Link')
         verbose_name_plural = _('Social Links')
 
+
 class FooterLink(AbstractLink):
     icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
-    link_edit = models.ForeignKey(LinkEdit, on_delete=models.SET_NULL, null=True, related_name='footer_links')
+    link_edit = models.ForeignKey(
+        LinkEdit, on_delete=models.SET_NULL, null=True, related_name='footer_links')
 
     class Meta:
         verbose_name = _('Footer Link')
         verbose_name_plural = _('Footer Links')
 
+
 class NavLink(AbstractLink):
-    link_edit = models.ForeignKey(LinkEdit, on_delete=models.SET_NULL, null=True, related_name='navbar_links')
-    
+    link_edit = models.ForeignKey(
+        LinkEdit, on_delete=models.SET_NULL, null=True, related_name='navbar_links')
+
     class Meta:
         verbose_name = _('Navigation Link')
         verbose_name_plural = _('Navigation Links')
@@ -230,8 +273,10 @@ class CollectionItemCommon(models.Model):
     image = models.ImageField(upload_to='images')
     created = models.DateTimeField()
     spotlight = models.BooleanField()
-    parent_section = models.ForeignKey(Section, on_delete=models.SET_NULL, null=True)
-    detail_view = models.CharField(max_length=64, default='portfolio.views.BaseEntryView', choices=views)
+    parent_section = models.ForeignKey(
+        SectionCommon, on_delete=models.SET_NULL, null=True)
+    detail_view = models.CharField(
+        max_length=64, default='portfolio:detail', choices=views)
 
     class Meta:
         verbose_name = _('Collection Item')
@@ -240,20 +285,24 @@ class CollectionItemCommon(models.Model):
     def __str__(self):
         return self.friendly_name
 
+
 class CollectionItem(AbstractTranslatable):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, editable=False)
     description = models.TextField()
-    common = models.ForeignKey(CollectionItemCommon, related_name='collection_items', on_delete=models.CASCADE, null=True)
+    common = models.ForeignKey(
+        CollectionItemCommon, related_name='collection_items', on_delete=models.CASCADE, null=True)
     image_alttext = models.TextField(default="")
+
     class Meta:
         verbose_name = _('Item Translation')
         verbose_name_plural = _('Item Translations')
 
-
     def save(self):
         self.slug = slugify(self.name)
         super().save()
+
+
 """ 
 class Project(models.Model):
     name = models.CharField(max_length=100)
@@ -300,16 +349,15 @@ class Photo(models.Model):
  """
 
 
-
 class Contact(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     email_address = models.EmailField()
     email_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email_address
 
-    
     def send_confirmation_email(self, token):
         if settings.DEV_MODE:
             url = f'{settings.PROTO}://{settings.HOST}:{settings.PORT}/contact/confirm-email/{self.id}/{token}'
@@ -325,6 +373,7 @@ class Contact(models.Model):
             fail_silently=False
         )
 
+
 class Message(models.Model):
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)
@@ -333,8 +382,7 @@ class Message(models.Model):
 
     def __str__(self):
         return f"{self.contact.email_address} - {self.subject}"
-    
-    
+
     def send(self):
         email = EmailMultiAlternatives(
             subject=self.subject,
@@ -356,4 +404,3 @@ class Message(models.Model):
         email.send(fail_silently=False)
         # TODO: Check if email was sent successfi;;y
         self.sent = True
-
