4337b3f8439a1e8ccac05fd089f5067f15adcc64
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-11 19:12:11 +0200

    reorder urls to not override defined paths with regex paths

--- homepage/homepage/urls.py
+++ homepage/homepage/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('portfolio.urls')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('portfolio.urls')),
 ]
