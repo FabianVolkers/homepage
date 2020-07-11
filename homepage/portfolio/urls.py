4ef53fb76ae1638f815b7750cf7043b836daf4df
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-11 19:14:51 +0200

    rearrange regex paths and add contact path

--- homepage/portfolio/urls.py
+++ homepage/portfolio/urls.py
@@ -6,32 +6,35 @@ from . import views
 
 app_name = 'portfolio'
 urlpatterns = [
-    re_path(r'^(?!(admin|media|projects|photography|lang))(?P<page_name>[\w|-]{0,}$)',
-            views.PageView.as_view(), name='page'),
-    path('contact', views.contact, name='contact'),
-    path('contact/thanks/<int:message_id>',
-         views.contacted, name='contact-thanks'),
-    path('contact/confirm-email/<str:contact_id>/<str:token>',
-         views.confirm_email, name='confirm-email'),
-    path('contact/unconfirmed-email/<str:contact_id>',
-         views.unconfirmed_email, name='unconfirmed-email'),
-    path('contact/send-confirmation-email/<str:contact_id>',
-         views.send_confirmation_email, name='send-confirmation-email'),
-    #path('projects', views.CollectionView.as_view(), name='projects-collection'),
-    # path('project/<slug:slug>', views.DetailView.as_view(),
-    #     name='project-detail'),
-    # path('photography', views.CollectionView.as_view(),
-    #     name='photography-collection'),
-    #path('photo/<slug:slug>', views.DetailView.as_view(), name='photo-detail'),
-    #path('imprint', views.ImprintView.as_view(), name='imprint'),
-    #path('privacy', views.PrivacyView.as_view(), name='privacy'),
+    re_path(r'^contact-view(?:/(?P<slug>[\w|-]{0,}))?',
+            views.ContactView.as_view(), name="contact"),
     path('lang', views.set_language, name='set-lang'),
-    re_path(r'^(?!(admin|media|privacy|imprint|lang]))(?P<section_slug>[\w|-]+$)',
-            views.CollectionView.as_view(), name='collection'),
     re_path(r'^(?![admin|media|lang])(?P<section_slug>[\w|-]+)/(?P<collectionitem_slug>[\w|-]+)',
             views.DetailView.as_view(), name='detail'),
-
+    re_path(r'^(?!(admin|media|privacy|imprint|lang]))(?P<section_slug>[\w|-]+$)',
+            views.CollectionView.as_view(), name='collection'),
+    re_path(r'^(?!(admin|media|projects|photography|lang))(?P<page_name>[\w|-]{0,}$)',
+            views.PageView.as_view(), name='page'),
 ]
+# path('contact', views.contact, name='contact'),
+# path('contact/thanks/<int:message_id>',
+"""   views.contacted, name = 'contact-thanks'),
+     path('contact/confirm-email/<str:contact_id>/<str:token>',
+         views.confirm_email, name = 'confirm-email'),
+     path('contact/unconfirmed-email/<str:contact_id>',
+         views.unconfirmed_email, name = 'unconfirmed-email'),
+     path('contact/send-confirmation-email/<str:contact_id>',
+         views.send_confirmation_email, name = 'send-confirmation-email'), """
+
+# path('projects', views.CollectionView.as_view(), name='projects-collection'),
+# path('project/<slug:slug>', views.DetailView.as_view(),
+#     name='project-detail'),
+# path('photography', views.CollectionView.as_view(),
+#     name='photography-collection'),
+# path('photo/<slug:slug>', views.DetailView.as_view(), name='photo-detail'),
+# path('imprint', views.ImprintView.as_view(), name='imprint'),
+# path('privacy', views.PrivacyView.as_view(), name='privacy'),
+
 
 if settings.DEV_MODE:
     urlpatterns += static(settings.MEDIA_URL,
