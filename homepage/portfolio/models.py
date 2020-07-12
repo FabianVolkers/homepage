152ebcd17c0e9922a8b40fbbc4c1185c8aa316e1
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-12 17:02:03 +0200

    remove duplicate fields, add comments

--- homepage/portfolio/models.py
+++ homepage/portfolio/models.py
@@ -9,10 +9,15 @@ from django.template.loader import get_template, render_to_string
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import render
+from django.http import QueryDict
+from django.urls import reverse
 
 from .validators import *
 from .translations import filter_translations
 
+"""
+A couple of hard coded choices for the database models.
+"""
 views = [
     ('portfolio:detail', 'Detail View'),
     ('portfolio:collection', 'Collection View'),
@@ -43,13 +48,12 @@ sociallink_positions = [
     ('deg180', '09:00'),
     ('deg225', '10:30')
 ]
-# for l in range()
+
 link_types = [
     ('page', 'Page'),
     ('section', 'Section'),
     ('external', 'External')
 ]
-# Create your models here.
 
 
 class Setting(models.Model):
@@ -58,6 +62,11 @@ class Setting(models.Model):
 
     def __str__(self):
         return f'{self.key}={self.value}'
+
+
+"""
+Icon model for external links
+"""
 
 
 class Icon(models.Model):
@@ -80,6 +89,11 @@ class Icon(models.Model):
         self.colour = self.colour_value
 
 
+"""
+Abstract Translatable model from which all models with translatable fields inherit
+"""
+
+
 class AbstractTranslatable(models.Model):
 
     lang = models.CharField(
@@ -93,6 +107,17 @@ class AbstractTranslatable(models.Model):
         return self.lang
 
 
+"""
+Models for website pages
+########################
+"""
+
+
+"""
+PageType model for the different types of pages supported
+"""
+
+
 class PageType(models.Model):
     name = models.CharField(max_length=24)
     template_name = models.CharField(max_length=64)
@@ -103,24 +128,33 @@ class PageType(models.Model):
     def __str__(self):
         return self.name
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+    def save(self, force_insert=False,
+             force_update=False, using=None, update_fields=None):
 
         if self.overwrite_view_name != None:
             self.view_name = self.overwrite_view_name
         else:
             self.view_name = f"portfolio:{self.name}"
 
-        return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
+        return super().save(force_insert=force_insert,
+                            force_update=force_update,
+                            using=using,
+                            update_fields=update_fields)
+
+
+"""
+PageCommon model for shared attributes between translations
+"""
 
 
 class PageCommon(models.Model):
     friendly_name = models.CharField(max_length=64)
-    template_name = models.CharField(max_length=64)
+
     page_type = models.ForeignKey(
         PageType, on_delete=models.SET_NULL, null=True, blank=True)
     footer_link = models.BooleanField(default=True)
-    footer_position = models.IntegerField(
-        unique=True, null=True, blank=True, choices=[(i, i) for i in range(10)])
+    footer_position = models.IntegerField(unique=True, null=True, blank=True,
+                                          choices=[(i, i) for i in range(10)])
 
     class Meta:
         verbose_name = _('Page')
@@ -130,14 +164,17 @@ class PageCommon(models.Model):
         return self.friendly_name
 
 
+"""
+Translatable Page model holding the page text translations
+"""
+
+
 class Page(AbstractTranslatable):
     name = models.CharField(max_length=24)
 
     slug = models.SlugField(null=True, blank=True)
     common = models.ForeignKey(
         PageCommon, on_delete=models.CASCADE, null=True)
-
-    # nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, default=1, null=True)
 
     class Meta:
         verbose_name = _('Single Page')
@@ -146,9 +183,21 @@ class Page(AbstractTranslatable):
     def __str__(self):
         return self.name
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+    def save(self, force_insert=False, force_update=False,
+             using=None, update_fields=None):
         super().save(force_insert, force_update, using, update_fields)
         self.slug = slugify(self.name)
+
+
+"""
+Models for page sections
+########################
+"""
+
+"""
+Section Type model for defining different section types
+e.g.: Collection, Spotlight, Contact
+"""
 
 
 class SectionType(models.Model):
@@ -158,6 +207,11 @@ class SectionType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+"""
+Section Common model for shared attributes between translated sections
+"""
 
 
 class SectionCommon(models.Model):
@@ -171,7 +225,6 @@ class SectionCommon(models.Model):
         null=True, blank=True,
         related_name='sections'
     )
-    # nav_link = models.ForeignKey(NavLink, on_delete=models.SET_NULL, default=1, null=True)
 
     class Meta:
         verbose_name = _('Page Section')
@@ -182,14 +235,19 @@ class SectionCommon(models.Model):
         return self.friendly_name
 
 
+"""
+Translatable section model holding section translations
+"""
+
+
 class Section(AbstractTranslatable):
-    # section_positions = range(1, len(Section.objects.all()))
+
     name = models.CharField(max_length=64)
     slug = models.SlugField()
     detail_slug = models.SlugField(null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    common = models.ForeignKey(
-        SectionCommon, related_name='sections', on_delete=models.CASCADE, null=True)
+    common = models.ForeignKey(SectionCommon, related_name='sections',
+                               on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = _('Section Translation')
@@ -199,22 +257,18 @@ class Section(AbstractTranslatable):
         return self.name
 
     def save(self):
-        """ TODO: Add support for default position
-        sections = Section.objects.order_by(position)
-        if self.position == None:
-            self.position = self.section_type.default_position """
-
         self.slug = slugify(self.name)
-        """ if self.common.nav_bar_link:
-            try:
-                NavLink.objects.get(name=self.slug)
-            except models.ObjectDoesNotExist:
-                position = len(NavLink.objects.all())
-                navlink = NavLink(
-                    name=self.slug, url=f'/#{self.slug}', position=position)
-                navlink.save()
-                self.common.nav_link = navlink """
         return super().save()
+
+
+"""
+Models for Navigation, Contact and Footer links
+###############################################
+"""
+
+"""
+Abstract Link model from which all other link models inherit
+"""
 
 
 class AbstractLink(models.Model):
@@ -236,25 +290,13 @@ class AbstractLink(models.Model):
     def __str__(self):
         return self.url
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        """ if self.link_for == 'section':
-            self.url = f"{self.page.slug}/#{self.section.slug}"
-        elif self.link_for == 'page':
-            self.url = f"/{self.page.objects.select_related('common').get()slug}"
-        elif self.link_for == 'external':
-            self.url = self.external """
-
-        return super().save(
-            force_insert=force_insert,
-            force_update=force_update,
-            using=using,
-            update_fields=update_fields
-        )
-
     class Meta:
         abstract = True
-        # verbose_name = 'ModelName'
-        # verbose_name_plural = 'ModelNames'
+
+
+"""
+Helper model for editing all page links in bulk
+"""
 
 
 class LinkEdit(models.Model):
@@ -264,35 +306,66 @@ class LinkEdit(models.Model):
         verbose_name_plural = _('Page Links')
 
 
+"""
+Social Link model for the contact circle
+"""
+
+
 class SocialLink(AbstractLink):
-    icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
+    icon = models.ForeignKey(Icon, on_delete=models.SET_NULL,
+                             null=True)
     link_edit = models.ForeignKey(
-        LinkEdit, on_delete=models.SET_NULL, null=True, related_name='social_links')
+        LinkEdit, on_delete=models.SET_NULL, null=True,
+        related_name='social_links')
     position = models.CharField(
-        max_length=6, null=True, blank=True, unique=True, choices=sociallink_positions)
+        max_length=6, null=True, blank=True, unique=True,
+        choices=sociallink_positions)
 
     class Meta:
         verbose_name = _('Social Link')
         verbose_name_plural = _('Social Links')
 
 
+"""
+Footer link model
+"""
+
+
 class FooterLink(AbstractLink):
     icon = models.ForeignKey(Icon, on_delete=models.SET_NULL, null=True)
     link_edit = models.ForeignKey(
-        LinkEdit, on_delete=models.SET_NULL, null=True, related_name='footer_links')
+        LinkEdit, on_delete=models.SET_NULL,
+        null=True, related_name='footer_links')
 
     class Meta:
         verbose_name = _('Footer Link')
         verbose_name_plural = _('Footer Links')
 
 
+"""
+Navigation Link model
+"""
+
+
 class NavLink(AbstractLink):
     link_edit = models.ForeignKey(
-        LinkEdit, on_delete=models.SET_NULL, null=True, related_name='navbar_links')
+        LinkEdit, on_delete=models.SET_NULL,
+        null=True, related_name='navbar_links')
 
     class Meta:
         verbose_name = _('Navigation Link')
         verbose_name_plural = _('Navigation Links')
+
+
+"""
+Models for Collection Items
+e.g.: Projects, Photos
+##################
+"""
+
+"""
+Collection Item Common model for shared attributes between translated items
+"""
 
 
 class CollectionItemCommon(models.Model):
@@ -313,12 +386,18 @@ class CollectionItemCommon(models.Model):
         return self.friendly_name
 
 
+"""
+Collection Item model for translatable items
+"""
+
+
 class CollectionItem(AbstractTranslatable):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, editable=False)
     description = models.TextField()
     common = models.ForeignKey(
-        CollectionItemCommon, related_name='collection_items', on_delete=models.CASCADE, null=True)
+        CollectionItemCommon, related_name='collection_items',
+        on_delete=models.CASCADE, null=True)
     image_alttext = models.TextField(default="")
 
     class Meta:
@@ -328,6 +407,16 @@ class CollectionItem(AbstractTranslatable):
     def save(self):
         self.slug = slugify(self.name)
         super().save()
+
+
+"""
+Models for the contact form
+###########################
+"""
+
+"""
+Contact model to enable email address confirmation
+"""
 
 
 class Contact(models.Model):
@@ -341,14 +430,16 @@ class Contact(models.Model):
         return self.email_address
 
     def send_confirmation_email(self, token):
+        url = reverse('portfolio:contact', args=['confirm'])
+        url = f"{url}?{QueryDict(f'id={self.id}&token={token}').urlencode()}"
         if settings.DEV_MODE:
-            url = f'{settings.PROTO}://{settings.HOST}:{settings.PORT}/contact-view/confirm?id={self.id}&token={token}'
+            url = f'{settings.PROTO}://{settings.HOST}:{settings.PORT}/{url}'
         else:
-            url = f'{settings.PROTO}://{settings.HOST}/contact/confirm-email/{self.id}/{token}'
-        print(url)
+            url = f'{settings.PROTO}://{settings.HOST}/{url}'
 
+        pages = Page.objects.all().select_related('common')
         pages = filter_translations(
-            Page.objects.all().select_related('common'), 'en').order_by('common__footer_position')
+            pages, 'en').order_by('common__footer_position')
 
         context = {
             'pages': pages,
@@ -367,6 +458,11 @@ class Contact(models.Model):
             html_message=html_email,
             fail_silently=False
         )
+
+
+"""
+Message model for storing messages until email confirmation
+"""
 
 
 class Message(models.Model):
@@ -393,23 +489,30 @@ class Message(models.Model):
 
         html_message = email_template.render(context=context)
 
+        headers = {
+            'Sender': 'noreply@fabianvolkers.com',
+            'From': f'{self.contact.name} via fabianvolkers.com <{self.contact.email_address}>',
+            'Reply-To': f'{self.contact.name} <{self.contact.email_address}>',
+            'To': f'Fabian Volkers <{settings.EMAIL_CONTACT_ADDRESS}>'
+        }
         email = EmailMultiAlternatives(
             subject=self.subject,
             from_email="Fabian Volkers <noreply@fabianvolkers.com>",
             to=[settings.EMAIL_CONTACT_ADDRESS],
             body=self.content,
-            headers={
-                'Sender': 'noreply@fabianvolkers.com',
-                'From': f'{self.contact.name} via fabianvolkers.com < {self.contact.email_address} >',
-                'Reply-To': f'{self.contact.name} < {self.contact.email_address} >',
-                'To': f'Fabian Volkers < {settings.EMAIL_CONTACT_ADDRESS} >'
-            }
+            headers=headers
         )
         email.attach_alternative(html_message, 'text/html')
         email.send(fail_silently=False)
 
         self.date_sent = datetime.datetime.now()
         self.sent = True
+
+
+"""
+Contact Response Action model for storing actions for response to user
+e.g.: resend confirmation email, delete personal information
+"""
 
 
 class ContactResponseAction(models.Model):
@@ -424,6 +527,12 @@ class ContactResponseAction(models.Model):
         return self.name
 
 
+"""
+Contact response common for shared attributes between translatable contact
+responses
+"""
+
+
 class ContactResponseCommon(models.Model):
     friendly_name = models.CharField(max_length=32)
     action = models.ForeignKey(
@@ -431,6 +540,11 @@ class ContactResponseCommon(models.Model):
 
     def __str__(self):
         return f"{self.friendly_name}"
+
+
+"""
+Contact response model for translatable contact responses
+"""
 
 
 class ContactResponse(AbstractTranslatable):
