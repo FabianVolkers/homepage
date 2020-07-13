497c3b2a930967df74b54410de317d7ad48c3c24
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-13 15:57:53 +0200

    add logging config

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import logging.config
 import os
 import random
 
@@ -88,6 +88,35 @@ else:
                 "PORT": os.getenv('DATABASE_PORT'),
             }
         } """
+    # Logging Configuration
+
+    # Clear prev config
+    LOGGING_CONFIG = None
+
+    # Get loglevel from env
+    LOGLEVEL = os.getenv('DJANGO_LOGLEVEL', 'info').upper()
+
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'console': {
+                'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+            },
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'console',
+            },
+        },
+        'loggers': {
+            '': {
+                'level': LOGLEVEL,
+                'handlers': ['console', ],
+            },
+        },
+    })
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/3.0/howto/static-files/
