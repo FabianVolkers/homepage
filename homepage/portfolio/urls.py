d9b4c788a1aca6e0578553302ac7031570c0d4c2
Fabian Volkers <fabian.volkers@gmail.com>
2020-07-15 13:28:40 +0200

    negative lookaheads in paths for media and static

--- homepage/portfolio/urls.py
+++ homepage/portfolio/urls.py
@@ -10,14 +10,20 @@ urlpatterns = [
     path('lang', views.set_language, name='set-lang'),
     re_path(r'^contact(?:/(?P<slug>[\w|-]{0,}))?',
             views.ContactView.as_view(), name="contact"),
-    re_path(r'^(?P<section_slug>[\w|-]+)/(?P<collectionitem_slug>[\w|-]+)',
+    re_path(r'^(?!(admin|media|static))(?P<section_slug>[\w|-]+)/(?P<collectionitem_slug>[\w|-]+)',
             views.DetailView.as_view(), name='detail'),
     re_path(r'^(?!(admin|media|privacy|imprint]))(?P<section_slug>[\w|-]+$)',
             views.CollectionView.as_view(), name='collection'),
-    re_path(r'^(?!(admin|media|projects|photography))(?P<page_name>[\w|-]{0,}$)',
+    re_path(r'^(?!(admin|media|static|projects|photography))(?P<page_name>[\w|-]{0,}$)',
             views.PageView.as_view(), name='page'),
 ]
 
+
 if settings.ENVIRONMENT == 'development':
+
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    print(settings.STATIC_URL, settings.MEDIA_URL)
+    print(settings.STATIC_ROOT, settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
